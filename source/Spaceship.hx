72e052a204d30963c3d46190f9fddbc2ec5e88eb
Green Lightning <GreenLightning.git@googlemail.com>
2014-04-24 00:11:42 +0200

    Teleportation, rotation and multiple key support.

--- source/Spaceship.hx
+++ source/Spaceship.hx
@@ -1,5 +1,6 @@
 import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.input.keyboard.FlxKey;
 
 class Spaceship extends FlxSprite {
 
@@ -13,16 +14,33 @@ class Spaceship extends FlxSprite {
 
 	override public function update():Void {
 		super.update();
-		if (FlxG.keys.justPressed.SPACE) {
-			animation.play("power");
-		}
-		if (FlxG.keys.justReleased.SPACE) {
-			animation.play("float");
-		}
-		if (FlxG.keys.pressed.SPACE) {
-			velocity.y -= 100 * FlxG.elapsed;
-		}
+		move();
 		limitVelocity(400);
+		stayInBounds();
+	}
+
+	private function move():Void {
+		var left = ["LEFT", "A"];
+		var right = ["RIGHT", "D"];
+		var power = ["UP", "W", "SPACE"];
+		if (FlxG.keys.anyPressed(left)) {
+			angle -= 360 / 2 * FlxG.elapsed;
+		}
+		if (FlxG.keys.anyPressed(right)) {
+			angle += 360 / 2 * FlxG.elapsed;
+		}
+		angle = (angle + 360) % 360;
+		if (FlxG.keys.anyPressed(power)) {
+			velocity.x += 100 * Math.sin(Math.PI / 180 * angle) * FlxG.elapsed;
+			velocity.y -= 100 * Math.cos(Math.PI / 180 * angle) * FlxG.elapsed;
+			if (animation.name != "power") {
+				animation.play("power");
+			}
+		} else {
+			if (animation.name != "float") {
+				animation.play("float");
+			}
+		}
 	}
 
 	private function limitVelocity(max:Float):Void {
@@ -30,6 +48,21 @@ class Spaceship extends FlxSprite {
 		if (lengthSquared > max * max) {
 			velocity.x = velocity.x * max * max / lengthSquared;
 			velocity.y = velocity.y * max * max / lengthSquared;
+		}
+	}
+
+	private function stayInBounds():Void {
+		if (x + width / 2 < 0) {
+			x += FlxG.width;
+		}
+		if (y + height / 2 < 0) {
+			y += FlxG.height;
+		}
+		if (x + width / 2 > FlxG.width) {
+			x -= FlxG.width;
+		}
+		if (y + height / 2 > FlxG.height) {
+			y -= FlxG.height;
 		}
 	}
 
